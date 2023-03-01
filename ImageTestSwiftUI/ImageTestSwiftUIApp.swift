dc13fc5a0c1dec8e32d6f9b463f4dc031944524d
Aleksandr Karpov <papadoster13@gmail.com>
2023-03-01 22:38:18 +0100

    Initial Commit

---
+++ ImageTestSwiftUI/ImageTestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageTestSwiftUIApp.swift
+//  ImageTestSwiftUI
+//
+//  Created by Александр Карпов on 01.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageTestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
